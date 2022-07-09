c699b0dac0a77e74fb3dd2434076c6637e34a846
Your Name <you@example.com>
2022-07-09 16:21:11 -0600

    W9D assignment added arrays and loops to display name and age

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let user_names = [`Jackson`, `noah`, `liam`, `lucas`, `benjamin`, `ethan`, `oliver`, `logan`, `jack`, `james`];
+let user_ages = [`10`, `12`, `14`, `16`, `18`, `20`, `22`, `24`, `26`, `28`];
+let counter = 0;
+while (counter < user_names.length) {
+    console.log(`name: ${user_names[counter]}`);
+    console.log(`age: ${user_ages[counter]}`);
+    counter++
+}
